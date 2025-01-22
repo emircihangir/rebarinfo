52d0d7a965dea5176a273ce2fbf98deb22aedb4e
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-22 21:31:38 +0300

    Added the CupertinoTabScaffold

--- lib/main-ios.dart
+++ lib/main-ios.dart
@@ -5,12 +5,53 @@ class IOSapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      home: CupertinoPageScaffold(
-        child: Center(
-          child: Text("IOS app"),
-        ),
-      ),
-    );
+    return CupertinoApp(
+        home: CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: const [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.circle_bottomthird_split), label: "Ağırlık"),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.arrow_2_circlepath), label: "Çap Dönüştürme"),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.recordingtape), label: "Donatı Seçimi"),
+      ]),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return const Page1View();
+          case 1:
+            return const Page2View();
+          case 2:
+            return const Page3View();
+
+          default:
+            return const Page1View();
+        }
+      },
+    ));
+  }
+}
+
+class Page1View extends StatelessWidget {
+  const Page1View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(child: SafeArea(child: Text("page 1")));
+  }
+}
+
+class Page2View extends StatelessWidget {
+  const Page2View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(child: SafeArea(child: Text("page 2")));
+  }
+}
+
+class Page3View extends StatelessWidget {
+  const Page3View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(child: SafeArea(child: Text("page 3")));
   }
 }
