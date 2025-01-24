2ad70c68d9c81e3fcc7c3f39a6e286928cc03551
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-24 22:04:03 +0300

    Renamed the ChangeNotifiers

--- lib/ios-views/page2.dart
+++ lib/ios-views/page2.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:rebarinfo/universal-functions.dart';
 
-class SPVpage2Model extends ChangeNotifier {
+class SelectedValueModel extends ChangeNotifier {
   int _selectedValue = 0;
 
   int get selectedValue => _selectedValue;
@@ -12,11 +12,11 @@ class SPVpage2Model extends ChangeNotifier {
   }
 }
 
-class Page2ResultModel extends ChangeNotifier {
-  double _Page2Result = 0;
-  double get Page2Result => _Page2Result;
-  set Page2Result(double value) {
-    _Page2Result = value;
+class ResultModel extends ChangeNotifier {
+  String _Result = "";
+  String get Result => _Result;
+  set Result(String value) {
+    _Result = value;
     notifyListeners();
   }
 }
