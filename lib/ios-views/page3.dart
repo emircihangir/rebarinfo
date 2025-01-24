77f23cffaa8069d98b3a56f49a00f9340504aff5
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-24 22:05:03 +0300

    Removed the unnecessary import

--- lib/ios-views/page3.dart
+++ lib/ios-views/page3.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:rebarinfo/universal-functions.dart';
-import 'package:photo_view/photo_view.dart';
 
 class Page3View extends StatelessWidget {
   const Page3View({super.key});
