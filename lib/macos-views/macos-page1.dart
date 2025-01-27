50c9acdd9f4f9d5632ee344610c2eb60c07f1431
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-27 21:49:27 +0300

    Added the TextField

--- lib/macos-views/macos-page1.dart
+++ lib/macos-views/macos-page1.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 class MacPage1View extends StatelessWidget {
   const MacPage1View({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        Center(
+          child: SizedBox(
+            width: 200,
+            child: MacosTextField(
+              placeholder: "Toplam Uzunluk (m)",
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
