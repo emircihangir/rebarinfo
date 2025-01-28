7bc5a16478c92eb0e95ca5422006093c0674bf35
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-28 16:59:36 +0300

    Removed the unnecessary widget

--- lib/macos-views/macos-page1.dart
+++ lib/macos-views/macos-page1.dart
@@ -37,65 +37,63 @@ class MacPage1View extends StatelessWidget {
           create: (context) => ResultModel(),
         )
       ],
-      builder: (context, child) => Center(
-        child: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: 150,
-                  child: MacosTextField(
-                    placeholder: "Toplam Uzunluk (m)",
-                    controller: uzunlukController,
-                    onChanged: (value) {
-                      try {
-                        final capDeger_parsed = int.parse(Provider.of<PopupModel>(context, listen: false).selectedValue.toString());
-                        final uzunlukDeger_parsed = double.parse(value.replaceAll(",", "."));
-                        Provider.of<ResultModel>(context, listen: false).result = page1_calculation(capDeger_parsed, uzunlukDeger_parsed);
-                      } on FormatException {
-                        return;
-                      }
-                    },
-                  ),
-                ),
-              ),
-            ),
-            Consumer<PopupModel>(
-              builder: (context, value, child) => SizedBox(
-                width: 100,
-                child: MacosPopupButton(
-                  items: capPickerValues
-                      .map(
-                        (e) => MacosPopupMenuItem(
-                          value: e,
-                          child: Text(e.toString()),
-                        ),
-                      )
-                      .toList(),
+      builder: (context, child) => Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 150,
+                child: MacosTextField(
+                  placeholder: "Toplam Uzunluk (m)",
+                  controller: uzunlukController,
                   onChanged: (value) {
-                    Provider.of<PopupModel>(context, listen: false).selectedValue = value;
-
                     try {
-                      final uzunlukDeger_parsed = double.parse(uzunlukController.text.replaceAll(",", "."));
-                      Provider.of<ResultModel>(context, listen: false).result = page1_calculation(value ?? 0, uzunlukDeger_parsed);
+                      final capDeger_parsed = int.parse(Provider.of<PopupModel>(context, listen: false).selectedValue.toString());
+                      final uzunlukDeger_parsed = double.parse(value.replaceAll(",", "."));
+                      Provider.of<ResultModel>(context, listen: false).result = page1_calculation(capDeger_parsed, uzunlukDeger_parsed);
                     } on FormatException {
                       return;
                     }
                   },
-                  hint: const Text("Çap seç"),
-                  value: Provider.of<PopupModel>(context, listen: false).selectedValue,
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Consumer<ResultModel>(
-                builder: (context, value, child) => Text("Sonuç: ${value.result.toStringAsFixed(2).replaceAll(".", ",")} kg"),
+          ),
+          Consumer<PopupModel>(
+            builder: (context, value, child) => SizedBox(
+              width: 100,
+              child: MacosPopupButton(
+                items: capPickerValues
+                    .map(
+                      (e) => MacosPopupMenuItem(
+                        value: e,
+                        child: Text(e.toString()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  Provider.of<PopupModel>(context, listen: false).selectedValue = value;
+
+                  try {
+                    final uzunlukDeger_parsed = double.parse(uzunlukController.text.replaceAll(",", "."));
+                    Provider.of<ResultModel>(context, listen: false).result = page1_calculation(value ?? 0, uzunlukDeger_parsed);
+                  } on FormatException {
+                    return;
+                  }
+                },
+                hint: const Text("Çap seç"),
+                value: Provider.of<PopupModel>(context, listen: false).selectedValue,
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Consumer<ResultModel>(
+              builder: (context, value, child) => Text("Sonuç: ${value.result.toStringAsFixed(2).replaceAll(".", ",")} kg"),
+            ),
+          )
+        ],
       ),
     );
   }
