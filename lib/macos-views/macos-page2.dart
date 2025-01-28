4c48ff0307858d8ccb646bea3f183a9f6d6cdea1
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-28 17:53:03 +0300

    Added the table

--- lib/macos-views/macos-page2.dart
+++ lib/macos-views/macos-page2.dart
@@ -21,6 +21,15 @@ class ResultModel extends ChangeNotifier {
   }
 }
 
+class TableValuesModel extends ChangeNotifier {
+  List<List>? _tableValues;
+  List<List>? get tableValues => _tableValues;
+  set tableValues(List<List>? value) {
+    _tableValues = value;
+    notifyListeners();
+  }
+}
+
 class MacPage2View extends StatelessWidget {
   const MacPage2View({super.key});
 
@@ -35,6 +44,9 @@ class MacPage2View extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (context) => ResultModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TableValuesModel(),
         )
       ],
       builder: (context, child) => Column(
@@ -51,7 +63,9 @@ class MacPage2View extends StatelessWidget {
                     try {
                       final capDeger_parsed = int.parse(Provider.of<PopupModel>(context, listen: false).selectedValue.toString());
                       final adetDeger_parsed = int.parse(value);
-                      Provider.of<ResultModel>(context, listen: false).result = page2_calculation(capDeger_parsed, adetDeger_parsed);
+                      final calculation = page2_calculation(capDeger_parsed, adetDeger_parsed);
+                      Provider.of<ResultModel>(context, listen: false).result = calculation;
+                      Provider.of<TableValuesModel>(context, listen: false).tableValues = page2_calculate_table_values(capDeger_parsed, adetDeger_parsed, calculation);
                     } on FormatException {
                       return;
                     }
@@ -76,10 +90,16 @@ class MacPage2View extends StatelessWidget {
                       .toList(),
                   onChanged: (value) {
                     Provider.of<PopupModel>(context, listen: false).selectedValue = value;
+                    print(value.runtimeType);
 
                     try {
                       final adetDeger_parsed = int.parse(adetController.text);
-                      Provider.of<ResultModel>(context, listen: false).result = page2_calculation(value ?? 0, adetDeger_parsed);
+                      final calculation = page2_calculation(value ?? 0, adetDeger_parsed);
+                      Provider.of<ResultModel>(context, listen: false).result = calculation;
+
+                      if (value != null) {
+                        Provider.of<TableValuesModel>(context, listen: false).tableValues = page2_calculate_table_values(value, adetDeger_parsed, calculation);
+                      }
                     } on FormatException {
                       return;
                     }
@@ -92,6 +112,45 @@ class MacPage2View extends StatelessWidget {
           ),
           Consumer<ResultModel>(
             builder: (context, value, child) => Text("Kesit alanı: ${value.result.toStringAsFixed(2).replaceAll(".", ",")} cm²"),
+          ),
+          const SizedBox(height: 8),
+          Expanded(
+            child: Consumer<TableValuesModel>(
+                builder: (context, value, child) => ListView.builder(
+                    itemCount: (value.tableValues?.length ?? -1) + 1,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return const ColoredBox(
+                          color: MacosColors.tertiaryLabelColor,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Text(
+                                "Adet",
+                                textAlign: TextAlign.center,
+                              )),
+                              Expanded(child: Text("Çap (mm)", textAlign: TextAlign.center)),
+                              Expanded(child: Text("Kesit Alanı (cm²)", textAlign: TextAlign.right))
+                            ],
+                          ),
+                        );
+                      } else {
+                        return ColoredBox(
+                          color: (index % 2) == 0 ? MacosColors.tertiaryLabelColor : MacosColors.transparent,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Text(
+                                value.tableValues?[index - 1][0].toString() ?? "",
+                                textAlign: TextAlign.center,
+                              )),
+                              Expanded(child: Text(value.tableValues?[index - 1][1].toString() ?? "", textAlign: TextAlign.center)),
+                              Expanded(child: Text(value.tableValues?[index - 1][2].toStringAsFixed(2).replaceAll(".", ",") ?? "", textAlign: TextAlign.right))
+                            ],
+                          ),
+                        );
+                      }
+                    })),
           )
         ],
       ),
