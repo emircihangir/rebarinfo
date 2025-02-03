7385cb5f96db41e1881be576555fb639f252d9c9
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-02-03 13:58:47 +0300

    Minor changes in main-android.dart

--- lib/main-android.dart
+++ lib/main-android.dart
@@ -12,12 +12,13 @@ class AndroidApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue), scaffoldBackgroundColor: Colors.white, appBarTheme: const AppBarTheme(backgroundColor: Colors.white)),
+      themeMode: ThemeMode.light,
       home: DefaultTabController(
         initialIndex: 0,
         length: 3,
         child: Scaffold(
           appBar: AppBar(
-            systemOverlayStyle: const SystemUiOverlayStyle(systemStatusBarContrastEnforced: false, systemNavigationBarContrastEnforced: false, systemNavigationBarColor: Colors.transparent, systemNavigationBarDividerColor: Colors.transparent, systemNavigationBarIconBrightness: Brightness.dark, statusBarIconBrightness: Brightness.dark, statusBarColor: Colors.transparent),
+            systemOverlayStyle: const SystemUiOverlayStyle(systemNavigationBarColor: Colors.white, statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark, systemNavigationBarIconBrightness: Brightness.dark),
             title: const TabBar(
               tabs: <Widget>[
                 Tab(child: FittedBox(child: Text('Ağırlık'))),
